290a82a2a1487ec958a83a7aea7918d84abc4bde
ropsutius <ropsutius@gmail.com>
2023-03-09 21:56:05 +0200

    Started the functionality of editing the bounding box. Unfinished

--- pages/SolvingCanvas.jsx
+++ pages/SolvingCanvas.jsx
@@ -1,7 +1,22 @@
-import { useEffect, useRef } from "react";
+import { startTransition, useEffect, useRef, useState } from "react";
 
-export default function SolvingCanvas(props) {
+export default function SolvingCanvas({
+  startingMousePos,
+  mousePos,
+  setStartingMousePos,
+  setMousePos,
+  leftOffset,
+  topOffset,
+  style,
+  width,
+  height,
+}) {
   const canvasRef = useRef(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingSide, setEditingSide] = useState(null);
+  const [prevStartingMousePos, setPrevStartingMousePos] = useState({});
+  const [prevMousePos, setPrevMousePos] = useState({});
 
   const getCoordinates = (event) => {
     const parentTransform =
@@ -12,14 +27,46 @@ export default function SolvingCanvas(props) {
     const yOffset = Number(array[1].slice(0, -3));
     const scale = Number(array[2].slice(6, -1));
 
-    const x = (event.touches[0].clientX - xOffset - props.leftOffset) / scale;
-    const y = (event.touches[0].clientY - yOffset - props.topOffset) / scale;
+    const x = (event.touches[0].clientX - xOffset - leftOffset) / scale;
+    const y = (event.touches[0].clientY - yOffset - topOffset) / scale;
 
     return { x, y };
   };
 
   const handleTouchStart = (event) => {
-    props.setStartingMousePos(getCoordinates(event));
+    const coordinates = getCoordinates(event);
+
+    if (
+      Object.keys(startingMousePos).length !== 0 &&
+      Object.keys(mousePos).length !== 0
+    ) {
+      setIsEditing(true);
+      if (
+        coordinates.x < Math.max(startingMousePos.x, mousePos.x) + 30 &&
+        coordinates.x > Math.max(startingMousePos.x, mousePos.x - 30)
+      ) {
+        setEditingSide("right");
+      } else if (
+        coordinates.x < Math.min(startingMousePos.x, mousePos.x) + 30 &&
+        coordinates.x > Math.min(startingMousePos.x, mousePos.x - 30)
+      ) {
+        setEditingSide("left");
+      } else if (
+        coordinates.y < Math.max(startingMousePos.y, mousePos.y) + 30 &&
+        coordinates.y > Math.max(startingMousePos.y, mousePos.y - 30)
+      ) {
+        setEditingSide("bottom");
+      } else if (
+        coordinates.y < Math.min(startingMousePos.y, mousePos.y) + 30 &&
+        coordinates.y > Math.min(startingMousePos.y, mousePos.y - 30)
+      ) {
+        setEditingSide("top");
+      } else {
+        setIsEditing(false);
+      }
+    }
+
+    setStartingMousePos(coordinates);
   };
 
   const handleTouchMove = (event) => {
@@ -31,7 +78,7 @@ export default function SolvingCanvas(props) {
     coords.y = coords.y < 0 ? 0 : coords.y;
     coords.y = coords.y > event.target.height ? event.target.height : coords.y;
 
-    props.setMousePos(coords);
+    setMousePos(coords);
   };
 
   useEffect(() => {
@@ -39,25 +86,46 @@ export default function SolvingCanvas(props) {
     const context = canvas.getContext("2d");
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    const topLeftX = Math.min(props.mousePos.x, props.startingMousePos.x);
-    const topLeftY = Math.min(props.mousePos.y, props.startingMousePos.y);
+    let topLeftX, topLeftY, width, height;
 
-    const width = Math.abs(props.mousePos.x - props.startingMousePos.x);
-    const height = Math.abs(props.mousePos.y - props.startingMousePos.y);
+    if (!isEditing) {
+      topLeftX = Math.min(mousePos.x, startingMousePos.x);
+      topLeftY = Math.min(mousePos.y, startingMousePos.y);
+
+      width = Math.abs(mousePos.x - startingMousePos.x);
+      height = Math.abs(mousePos.y - startingMousePos.y);
+    } else {
+      console.log("first");
+      topLeftX = Math.min(prevMousePos.x, prevStartingMousePos.x);
+      topLeftY = Math.min(prevMousePos.y, prevStartingMousePos.y);
+
+      width = Math.abs(prevMousePos.x - prevStartingMousePos.x);
+      height = Math.abs(prevMousePos.y - prevStartingMousePos.y);
+
+      switch (editingSide) {
+        case "top":
+          topLeftY = mousePos.y;
+          height -=
+            mousePos.y - Math.min(prevMousePos.y, prevStartingMousePos.y);
+          break;
+      }
+    }
 
     context.strokeStyle = "rgba(255, 0, 0, 1)";
     context.beginPath();
     context.rect(topLeftX, topLeftY, width, height);
-    //context.rect(0, 0, 100, 100);
     context.stroke();
-  }, [props.mousePos]);
+
+    setPrevMousePos(mousePos);
+    setPrevStartingMousePos(startingMousePos);
+  }, [mousePos]);
 
   return (
     <canvas
       ref={canvasRef}
-      style={props.style}
-      width={props.width}
-      height={props.height}
+      style={style}
+      width={width}
+      height={height}
       onTouchStart={handleTouchStart}
       onTouchMove={handleTouchMove}
     ></canvas>
